d56f9aa371f91c52f3491ea3260f935c30d40363
DESKTOP-16Q7AG9\Rudresh Tiwari <rudreshtiwari57@gmail.com>
2025-03-24 23:58:35 +0530

    some more changes

--- src/java_print/Java_print.java
+++ src/java_print/Java_print.java
@@ -15,11 +15,26 @@ public class Java_print {
 		System.out.println("hello ");
 		System.out.println("World !");
 //		----------------------------using printf
-//		this used to print formated line on the console
+//		this used to print formated line on the console and don't add new line at the end
+
+//				-------------------------------------------------------------------------------------------------------------
+//				| Format Specifier	|		Description						|		Example									|
+//				-------------------------------------------------------------------------------------------------------------
+//				|	%d				|	Integer (decimal)					|	System.out.printf("Age: %d", 25);			|
+//				|	%f				|	Floating-point number				|	System.out.printf("Price: %.2f", 99.99);	|
+//				|	%s				|	String								|	System.out.printf("Name: %s", "Rudresh");	|
+//				|	%c				|	Character							|	System.out.printf("Grade: %c", 'A');		|
+//				|	%b				|	Boolean								|	System.out.printf("Status: %b", true);		|
+//				|	%x				|	Hexadecimal (integer)				|	System.out.printf("Hex: %x", 255);			|
+//				|	%o				|	Octal (integer)						|	System.out.printf("Octal: %o", 8);			|
+//				|	%e				|	Scientific notation (exponential)	|	System.out.printf("Value: %e", 1234.567);	|
+//				-------------------------------------------------------------------------------------------------------------
+		
+		
 		String name = "Rudresh Tiwari";
 		int age = 24;
 		System.out.printf("Hello, \nMy name is %s and age %d",name,age);
-		System.out.printf("Hello, \nMy name is %s and age %d",name,age);
+		
 	}
 
 }
