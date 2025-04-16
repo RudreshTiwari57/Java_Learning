4b8040f0da2e379678c1e0b1ebf17ce74168dcfa
DESKTOP-16Q7AG9\Rudresh Tiwari <rudreshtiwari57@gmail.com>
2025-04-16 23:06:13 +0530

    lru changes

--- src/java_collections/lrucache/LRUcache.java
+++ src/java_collections/lrucache/LRUcache.java
@@ -3,16 +3,18 @@ package java_collections.lrucache;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class LRUcache<K,V> extends LinkedHashMap<K,V> {
-    private final int capacity;
-
-    public LRUcache(int capacity) {
-        super(capacity, 0.75f, true); // accessOrder = true
-        this.capacity = capacity;
+class LRUcache<K,V> extends LinkedHashMap <K,V>
+{
+    private final int cap;
+    public LRUcache(int cap)
+    {
+        super(cap,0.75f,true);
+        this.cap = cap;
     }
 
     @Override
-    protected boolean removeEldestEntry(Map.Entry<K,V> eldest) {
-        return size() > capacity;
+    public boolean removeEldestEntry(Map.Entry<K,V> entry)
+    {
+        return size()>cap;
     }
 }
--- src/java_collections/lrucache/TestLRU.java
+++ src/java_collections/lrucache/TestLRU.java
@@ -1,21 +1,21 @@
 package java_collections.lrucache;
 
+
 import java.util.Map;
 
 public class TestLRU {
-    public static void main(String[] args) {
-        LRUcache<Integer,String> lrUcache = new LRUcache<>(4);
-        lrUcache.put(1,"A");
-        lrUcache.put(2,"B");
-        lrUcache.put(3,"C");
 
-        lrUcache.get(1);
-        lrUcache.put(4,"D");
-        lrUcache.put(5,"E");
-        for(Map.Entry<Integer, String> entry:lrUcache.entrySet())
+    public static void main(String[] args) {
+        LRUcache<String,Integer> lrUcache = new LRUcache<>(3);
+        lrUcache.put("A",1);
+        lrUcache.put("B",2);
+        lrUcache.put("C",3);
+        lrUcache.get("B");
+        lrUcache.put("D",4);
+        System.out.println(lrUcache.keySet());
+        for(Map.Entry<String,Integer> entry: lrUcache.entrySet())
         {
             System.out.println(entry.getValue());
         }
-
     }
 }
